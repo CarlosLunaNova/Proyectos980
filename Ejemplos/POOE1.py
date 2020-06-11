26f2c38698edb3a3de314535cbc5fada6729cf2c
usac201602491 <2931416571705@ingenieria.usac.edu.gt>
2020-06-11 11:19:52 -0600

    POO1

---
+++ Ejemplos/POOE1.py
@@ -0,0 +1,7 @@
+'''
+Cada objetiene sus atributos y sus funciones propias de cada uno
+todos los objetos de la misma clase poseen los mismos atributos y funciones
+pero cada parametro varia entre estos
+
+Un objeto es una coleccion de datos y funciones ordenados en una jerarquia
+'''
