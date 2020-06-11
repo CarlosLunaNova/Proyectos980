ae227a8b57f2d55c418f44b9be412e545216d68c
usac201602491 <2931416571705@ingenieria.usac.edu.gt>
2020-06-11 12:53:49 -0600

    Class

--- Ejemplos/POOE1.py
+++ Ejemplos/POOE1.py
@@ -1,7 +1,37 @@
-'''
-Cada objetiene sus atributos y sus funciones propias de cada uno
-todos los objetos de la misma clase poseen los mismos atributos y funciones
-pero cada parametro varia entre estos
 
-Un objeto es una coleccion de datos y funciones ordenados en una jerarquia
-'''
+# Funcion --> Metodo
+# Variable --> Atributo
+
+# Clase tipo Fecha
+class Fecha(object):
+    #Constructor
+    def __init__(self, dia, mes, anio):
+        # Crear variables globales dentro de la clase
+        # con la informacion que era local dentro
+        # del metodo constructor
+        self.day = dia
+        self.month = mes
+        self.year = anio
+    
+    #Devolver la fecha como una tupla
+    def mostrarFecha(self):
+        x = (self.day,self.month,self.year)
+        return x
+    
+    #Cambiar la fecha guardada
+    def cambiarFecha(self, dia, mes, anio):
+        self.day = dia
+        self.month = mes
+        self.year = anio
+
+    # Redefine la conversion del objeto a string   
+    def __str__(self):
+        return str(str(self.day)+"/"+str(self.month)+"/"+str(self.year))
+
+    # Devuelve la representacion haciendo una sobrecarga de operador
+    def __repr__(self):
+        return self.__str__()        
+
+miCumple = Fecha(11,12,1997)
+revolucion = Fecha(20,10,1944)
+independencia = Fecha(15,9,1821)
